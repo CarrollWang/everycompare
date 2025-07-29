fc13c594712c68c0c8892c5da0edd6df8f10b26f
unknown <carroll>
2025-07-29 23:03:11 +0800

    添加google 分析代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4ZHH6QJQKN"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4ZHH6QJQKN');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased">
         {children}
       </body>
